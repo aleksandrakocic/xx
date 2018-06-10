9188eae841dcb3ffb28ffdef9a6f30d78737174b
aleksandrakocic <32391316+aleksandrakocic@users.noreply.github.com>
2018-06-10 17:00:21 +0200

    routes

--- xxx/src/main.js
+++ xxx/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+window.Vue = Vue;
+window.VueRouter = VueRouter;
+import VueRouter from 'vue-router';
+import router from '../routes';
+Vue.use(VueRouter);
+
 Vue.config.productionTip = false
 
 new Vue({
